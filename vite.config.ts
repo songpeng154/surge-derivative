a973529d529b2d6ede9c6425e49930881eb76eac
tudo <419626398@qq.com>
2024-11-18 15:16:30 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    plugins: [ vue() ],
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/main.ts'),
+            formats: [ 'es', 'umd' ]
+        },
+        rollupOptions: {
+            external: [ 'vue' ],
+            output: [
+                {
+                    format: 'es',
+                    preserveModules: true,
+                    //配置打包根目录
+                    dir: 'es',
+                    preserveModulesRoot: 'src'
+                }
+            ]
+        },
+    },
 })
